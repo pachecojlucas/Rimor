64d7a319ed9c4542fa3b64f9a0469d00b3986df5
pachecojlucas <112329254+pachecojlucas@users.noreply.github.com>
2022-11-28 16:52:27 -0300

    Update Rimor.py

--- Rimor.py
+++ Rimor.py
@@ -153,7 +153,7 @@ def app():
                         tamanho=None
                 
                 else:
-                        tamanho=None
+                        tamanho=4000
 
                 #histograma mat gen
                 st.write("Histograma - Frequência do" , label, "dos indivíduos dentro de cada material genético")
@@ -203,7 +203,7 @@ def app():
                         label2 = 'DAP (cm)' 
                 st.write("Gráfico de barras" ,label2 ,"médio de cada material genético")
 
-                Barras_medias = px.bar(tabela_resumo, x='Material genético', y= tabela_resumo[media].round(2),  width=None,color ='Sobrevivência %',
+                Barras_medias = px.bar(tabela_resumo, x='Material genético', y= tabela_resumo[media].round(2),  width=4000,color ='Sobrevivência %',
                 text_auto=True,
                 labels={'y': label2})
                                 
